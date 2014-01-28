61caa4a40ee7167a324dfb9fdf880753c613d833
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-01-28 11:57:55 -0800

    Relative link for bio pages.

--- scripts/muster.faculty-listing.js
+++ scripts/muster.faculty-listing.js
@@ -14,7 +14,7 @@
 	'use strict';
 
 	var people,
-		LINK = "http://education.ucsb.edu/research-faculty/bio?first=%s&last=%s",
+		LINK = "/research-faculty/bio?first=%s&last=%s",
 		NAME = '<h3><a href="%s">%s <span class=surname>%s</span></a></h3>',
 		IMG = '<img src="http://education.ucsb.edu/drupal7/sites/default/files/faculty_photos/%s.jpg" alt="%s %s %s"/>';
 
