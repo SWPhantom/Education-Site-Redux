94e5b104a53a6834c64ed2140e4daa5e800a2902
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-02-04 14:07:33 -0800

    Reverting back to the other code.

--- scripts/muster.faculty-spotlight.js
+++ scripts/muster.faculty-spotlight.js
@@ -24,33 +24,63 @@
  *
  */
 
-(function ($) {
-    
-    'use strict';
-    
-    var $facultySpotlight;
-    
-    $(document).ready(muster('ggsedb').query({
-        select: 'id, first_name, last_name, faculty_listing_category, biography',
-        from: 'profile',
-        where: 'id is not null'
-    }, function () {
-        
-        var randomIndex, person, $spotlightContent;
-        
-        randomIndex = Math.floor(this.results.length * Math.random());
-        person = this.results[randomIndex];
-        
-        $spotlightContent = $('<div>');
-        $spotlightContent.append($('<h3>' + person.first_name + ' ' + person.last_name + '</h3>'));
-        $spotlightContent.append($('<p>' + person.biography.slice(0, 150) + '</p>'));
-        
-        $(function () {
-            if ($facultySpotlight === undefined) {
-                $facultySpotlight = $('#facultySpotlight');
-            }
-            $facultySpotlight.append($spotlightContent);
-        });
+ (function ($) {
+		'use strict';
+		var people = [];
+		$(document).ready(muster('ggsedb').query({
+				select: 'id, first_name, last_name, faculty_listing_category, biography',
+				from: 'profile',
+				where: 'id is not null',
+				order: 'id asc',
+				limit: '1'
+		}, function () {
+				var IMG = '<img src="http://education.ucsb.edu/drupal7/sites/default/files/faculty_photos/%s.jpg" alt="%s %s"/>';
+				var NAME = '<a href="%s">%s <span class=surname>%s</span></a>';
+				var LINK = "/research-faculty/bio?first=%s&last=%s";
+				var pic, name, link;
+				this.toTable(
+				[
+						[
+								'',
 
-    }))
+						function () {
+								link = LINK.replace('%s', this.first_name).replace('%s', this.last_name);
+								pic = $(IMG
+										.replace('%s', (this.first_name + this.last_name).toLowerCase().replace(/[^a-z]/g, ''))
+										.replace('%s', this.first_name)
+										.replace('%s', this.last_name));
+								
+								pic.bind('error', function () {
+								this.src = 'http://education.ucsb.edu/drupal7/sites/default/files/faculty_photos/faculty-placeholder.gif'
+								});
+								name = $(NAME
+										.replace('%s', link)
+										.replace('%s', this.first_name)
+										.replace('%s', this.last_name));
+								
+								name.find('a').prepend(pic);
+								var temp = "";
+								//console.log(pic.img.outerHTML);
+								return pic;
+								return name;
+								/*return $('<a>').attr(
+										'href', [
+										'/research-faculty/bio?first=',
+								this.first_name,
+										'&last=',
+								this.last_name].join('')).text([this.first_name, this.last_name].join(' '));*/
+						}],
+
+						[
+								'',
+
+						function () {
+								var html, dl;
+								html = $('<div>'); // wrap it for appending
+								var str1 = this.biography.slice(0, 150) + "... <b>read more</b>";
+								html.append(str1 || null);
+								return html.html(); // unwrap it
+						}]
+				], '#facultySpotlight')
+		}))
 }(jQuery));
