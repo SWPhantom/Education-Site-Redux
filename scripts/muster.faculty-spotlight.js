9e385204e9ac2e15c5fc32559b6c1209a7bf7cbc
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-02-04 13:32:51 -0800

    Rudimentary, non-random spotlight implemented.

--- scripts/muster.faculty-spotlight.js
+++ scripts/muster.faculty-spotlight.js
@@ -24,114 +24,63 @@
  *
  */
 
- 
-///*
-(function ($) {
-	'use strict';
-	var people = [];
-	$(document).ready(muster('ggsedb').query({
-		select: 'id, first_name, last_name, faculty_listing_category, biography',
-		from: 'profile',
-		where: 'id is not null',
-		order: 'RANDOM()',
-		limit: '1'
-	}, function () {
-
-	this.toTable(
-			[
+ (function ($) {
+		'use strict';
+		var people = [];
+		$(document).ready(muster('ggsedb').query({
+				select: 'id, first_name, last_name, faculty_listing_category, biography',
+				from: 'profile',
+				where: 'id is not null',
+				order: 'id asc',
+				limit: '1'
+		}, function () {
+				var IMG = '<img src="http://education.ucsb.edu/drupal7/sites/default/files/faculty_photos/%s.jpg" alt="%s %s"/>';
+				var NAME = '<a href="%s">%s <span class=surname>%s</span></a>';
+				var LINK = "/research-faculty/bio?first=%s&last=%s";
+				var pic, name, link;
+				this.toTable(
 				[
-					'Test1',
-			function () {
-							return $('<a>').attr(
-								'href',
-								[
-									'/research-faculty/bio?first=',
-									this.first_name,
-									'&last=',
-									this.last_name
-								].join('')
-							).text([this.last_name, this.first_name].join(', '));
-						}
-				],
+						[
+								'',
 
-			[
-				'Test2',
+						function () {
+								link = LINK.replace('%s', this.first_name).replace('%s', this.last_name);
+								pic = $(IMG
+										.replace('%s', (this.first_name + this.last_name).toLowerCase().replace(/[^a-z]/g, ''))
+										.replace('%s', this.first_name)
+										.replace('%s', this.last_name));
+								
+								pic.bind('error', function () {
+								this.src = 'http://education.ucsb.edu/drupal7/sites/default/files/faculty_photos/faculty-placeholder.gif'
+								});
+								name = $(NAME
+										.replace('%s', link)
+										.replace('%s', this.first_name)
+										.replace('%s', this.last_name));
+								
+								name.find('a').prepend(pic);
+								var temp = "";
+								//console.log(pic.img.outerHTML);
+								return pic;
+								return name;
+								/*return $('<a>').attr(
+										'href', [
+										'/research-faculty/bio?first=',
+								this.first_name,
+										'&last=',
+								this.last_name].join('')).text([this.first_name, this.last_name].join(' '));*/
+						}],
 
-				function () {
-					var html, dl;
-					html = $('<div>'); // wrap it for appending
-					html.append(this.biography || null);
-					return html.html(); // unwrap it
-				}
-			]
-		], '#facultySpotlight')
+						[
+								'',
 
-	/*
-		people.push(this);
-		$("#facultySpotlight").append(this);*/
-	}))
-/*
-	$(document).ready(function output(peopleList){
-		var container = document.getElementById("facultySpotlight");
-		var person = peopleList[Math.floor(Math.random()*peopleList.length)];
-		var div = document.createElement("div");
-		div.innerHTML = "Hats";
-		$(container).append(div);
-	})
-	*/
-	
-	//$("#facultySpotlight").append("TEST");
-	//document.getElementById('facultySpotlight').innerHTML = '<b>this will appear bold</b>';
-	
-	
-	//$(document).ready(output(people));
+						function () {
+								var html, dl;
+								html = $('<div>'); // wrap it for appending
+								var str1 = this.biography.slice(0, 150) + "... <b>read more</b>";
+								html.append(str1 || null);
+								return html.html(); // unwrap it
+						}]
+				], '#facultySpotlight')
+		}))
 }(jQuery));
-//*/
-
-
-//=================================================================================================================================
-/*
-(function ($) {
-
-	'use strict';
-
-	muster('ggsedb').query({
-		select: 'id, first_name, last_name, instructor_code',
-		from: 'profile',
-		where: 'instructor_code is not null',
-		order: 'last_name asc'
-	}, function () {
-		this.toTable(
-			[
-				[
-					'Instructor',
-			function () {
-							return $('<a>').attr(
-								'href',
-								[
-									'/research-faculty/bio?first=',
-									this.first_name,
-									'&last=',
-									this.last_name
-								].join('')
-							).text([this.last_name, this.first_name].join(', '));
-						}
-				],
-
-			[
-				'Code',
-
-				function () {
-					var html, dl;
-					html = $('<div>'); // wrap it for appending
-					html.append(this.instructor_code || null);
-					return html.html(); // unwrap it
-				}
-			]
-		], '#facultySpotlight') // Target container for table (<div id="currentlyFunded"></div>)
-	});
-}(jQuery));
-
-
-*/
-
