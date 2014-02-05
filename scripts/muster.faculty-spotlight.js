26c529137914de2a973e7da054f314d14fd7664f
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-02-05 10:28:16 -0800

    Changed the exception handling to not be addressed during the query.

--- scripts/muster.faculty-spotlight.js
+++ scripts/muster.faculty-spotlight.js
@@ -32,13 +32,15 @@
 		$(document).ready(muster('ggsedb').query({
 				select: 'id, first_name, last_name, faculty_listing_category, biography',
 				from: 'profile',
-				where: "id is not null and faculty_listing_category = 'Academic Senate Faculty' and last_name is not 'Marcoulides'" //Added a Marcoulides exception.
+				where: "id is not null and faculty_listing_category = 'Academic Senate Faculty'"
 		}, function () {
 				
 				var randomIndex, person, $spotlightContent;
 				//XXX: Need to add exception handling: if the picture URL is 404, reroll the random number.
-				randomIndex = Math.floor(this.results.length * Math.random());
-				person = this.results[randomIndex];
+				while(person.last_name.toLowerCase() === 'marcoulides'){
+					randomIndex = Math.floor(this.results.length * Math.random());
+					person = this.results[randomIndex];
+				}
 				
 				$spotlightContent = $('<div>');
 				$spotlightContent.append($('<img src="http://education.ucsb.edu/drupal7/sites/default/files/faculty_photos/' + (person.first_name + person.last_name).toLowerCase().replace(/[^a-z]/g, '') + '.jpg"/>'));
