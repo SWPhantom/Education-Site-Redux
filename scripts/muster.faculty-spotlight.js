943d694ec9d9586a07c0c4688a95095f3318fdd7
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-02-05 10:33:26 -0800

    Added John Yun exception.

--- scripts/muster.faculty-spotlight.js
+++ scripts/muster.faculty-spotlight.js
@@ -39,7 +39,7 @@
 				//XXX: Need to add exception handling: if the picture URL is 404, reroll the random number.
 				randomIndex = Math.floor(this.results.length * Math.random());
 				person = this.results[randomIndex];
-				while(person.last_name.toLowerCase() === 'marcoulides'){
+				while(person.last_name.toLowerCase() === 'marcoulides' || person.last_name.toLowerCase() === 'yun'){
 					randomIndex = Math.floor(this.results.length * Math.random());
 					person = this.results[randomIndex];
 				}
