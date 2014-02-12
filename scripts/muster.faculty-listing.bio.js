ee78893db505d26975ef707751d48f94205dd2c4
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-02-12 11:13:49 -0800

    Space doesn't work. Trying '\n'.

--- scripts/muster.faculty-listing.bio.js
+++ scripts/muster.faculty-listing.bio.js
@@ -280,7 +280,7 @@
 				[departmentLink(person.department1), departmentLink(person.department2)]
 			],
 			[
-				' ',
+				'\n',
 				[
 					link(person.personal_website, '-Personal Website'),
 					link(person.group_research_website, '-Group Research Website'),
