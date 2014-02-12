83f2b30224ae2a7f823111ea59a43abe6dec7cfb
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-02-12 12:20:51 -0800

    Made the heading text disappear. Whoops. Gonna have to use css.
    Also trouble with selectors.

--- scripts/muster.faculty-listing.bio.js
+++ scripts/muster.faculty-listing.bio.js
@@ -138,7 +138,7 @@
 		}
 
 		// if we made it this far, there is data to be displayed so set the dt
-		dl.append($('<dt id="' + label + '">'));
+		dl.append($('<dt id="' + label + '">')).append(label);
 
 		$.each(value, function () {
 			if (this) {
